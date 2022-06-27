7025dabf218f49bccc563662cf0c89991fb64d72
nurbxfit <root.toor97@gmail.com>
2022-06-27 13:02:19 +0800

    add video content component

---
+++ src/app/shared-components/video-content/video-content.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-video-component',
+    templateUrl: 'video-content.component.html',
+    styleUrls: ['video-content.component.scss']
+})
+export class VideoContentComponent implements OnInit{
+    constructor(){}
+    ngOnInit(): void {
+        
+    }
+}
