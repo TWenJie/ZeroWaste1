6bd90283770696aeebb636b83eb5c4bdd4a8c2b9
nurbxfit <root.toor97@gmail.com>
2022-06-28 00:18:35 +0800

    fix bad UI styles in android emulator

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+
+
+export interface CapturedPhoto {
+  filepath: string;
+  webviewPath: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PhotoService {
+  constructor() {}
+
+  public async snapPicture() {
+    //take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: false, //set to true when release
   serviceURI: 'https://zerowaste.cs.usm.my/spebaapi',
 };
 
