2188aa36b6ea20c7ae8e2499d3a9eeff48d4074d
nurbxfit <root.toor97@gmail.com>
2022-07-18 01:12:02 +0800

    add ezwc service

---
+++ src/app/services/feeds-ezwc.service.ts
@@ -0,0 +1,96 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, from, Observable, of } from "rxjs";
+import { map, switchMap, take, tap } from "rxjs/operators";
+import { environment } from "src/environments/environment";
+import { EZWCFeed } from "../interfaces/feeds.interface";
+import { PaginationOptions, PaginationResponse } from "../interfaces/pagination.interface";
+import { CachingService } from "./caching.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class FeedsEZWCService{
+    private _API_URL = environment.serviceURI + '/posts/ezwc';
+
+    protected _feeds$: BehaviorSubject<any[]> = new BehaviorSubject([]);
+    
+    constructor(
+        protected http: HttpClient,
+        protected cachingService: CachingService,
+    ){}
+
+    get feeds() {
+        return this._feeds$.asObservable();
+    }
+
+
+    getData(url:string,forceRefresh:boolean = false): Observable<any>{
+        if(forceRefresh){
+          return this.requestAndCache(url);
+        }else{
+          const storedValue =  from(this.cachingService.getCachedRequest(url));
+          return storedValue.pipe(
+            switchMap(result=>{
+              if(!result) return this.requestAndCache(url);
+              return of(result);
+            })
+          );
+        } 
+    }
+    
+    genPaginationURL(namespace,options):string{
+        const url = `${this._API_URL}/${namespace}?limit=${options.limit}&page=${options.page}`;
+        return url;
+    }
+
+    requestAndCache(url):Observable<any>{
+        return this.http.get(url).pipe(
+          tap(res=>{
+            this.cachingService.cacheRequests(url,res);
+          })
+        );
+    }
+
+
+    paginate(options: PaginationOptions, forceRefresh?: boolean): Observable<any> {
+        let tempResponse: PaginationResponse<EZWCFeed>;
+        const url = this.genPaginationURL('',options);
+        return this.getData(url,forceRefresh).pipe(
+        switchMap((response)=>{
+            tempResponse = response;
+            return this.feeds;
+        }),
+        take(1),
+        map((ezwcFeed:EZWCFeed[])=>{
+            if(options.page == 0){
+            ezwcFeed = [];
+            }
+            ezwcFeed = (tempResponse.results.length > 0)? ezwcFeed.concat(tempResponse.results) : ezwcFeed;
+            tempResponse.results = ezwcFeed;
+            return tempResponse;
+        }),
+        tap((response:PaginationResponse<EZWCFeed>)=>{
+            this._feeds$.next(response.results);
+        })
+        )
+    }
+
+    create(feeds: Partial<EZWCFeed>): Observable<EZWCFeed[]> {
+        let newFeed: EZWCFeed;
+        return this.http.post(this._API_URL,feeds).pipe(
+            switchMap((response:EZWCFeed)=>{
+                newFeed = response;
+                return this.feeds;
+            }),
+            take(1),
+            map((feeds:EZWCFeed[])=>{
+                feeds = feeds.concat(newFeed);
+                return feeds;
+            }),
+            tap((feeds:EZWCFeed[])=>{
+                this._feeds$.next(feeds);
+            })
+        )
+    }
+}
