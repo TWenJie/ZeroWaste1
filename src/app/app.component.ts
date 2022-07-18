9453c96fa209767a949627ca138647852dacc08b
nurbxfit <root.toor97@gmail.com>
2022-07-19 03:05:20 +0800

    back button exit app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { App } from '@capacitor/app';
 import { Directory, Filesystem } from '@capacitor/filesystem';
-import { Platform } from '@ionic/angular';
+import { IonRouterOutlet, Platform } from '@ionic/angular';
 import { CachingService } from './services/caching.service';
 @Component({
   selector: 'app-root',
@@ -8,11 +9,17 @@ import { CachingService } from './services/caching.service';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild(IonRouterOutlet, { static : true }) routerOutlet: IonRouterOutlet;
   private youtubeApiLoaded: boolean = false;
   constructor(
     private cachingService: CachingService,
     private platform: Platform,
   ) {
+    this.platform.backButton.subscribeWithPriority(-1,()=>{
+      if(!this.routerOutlet.canGoBack()){
+        App.exitApp();
+      }
+    })
     this.cachingService.initStorage();
     this.initYoutubeApi();
     // this.initImageCacheDir();
