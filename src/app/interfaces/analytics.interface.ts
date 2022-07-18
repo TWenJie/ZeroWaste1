f4364ff56f094f5e86c2a3a0ce1ae24ee37d8be1
nurbxfit <root.toor97@gmail.com>
2022-07-18 15:35:42 +0800

    add analytics interface

---
+++ src/app/interfaces/analytics.interface.ts
@@ -0,0 +1,10 @@
+export enum FeedEventTypes{
+    ViewAPost = "View Post",
+    ViewEvent = "View Calendar",
+    LikePost = "Like Post",
+    ClickLink = "Click Links",
+    CreatePost = "Create Post",
+    CreateComment = "Comment",
+    WatchVideo = "Watch Video",
+    PaginateList = 'Scroll List',
+}
